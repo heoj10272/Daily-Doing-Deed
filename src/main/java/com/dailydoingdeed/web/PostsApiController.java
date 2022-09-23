4452e9659eaf43173571176d7fef8247747d82e9
JHS <wnghdtjr129@naver.com>
2022-09-23 14:26:17 +0900

    [22.09.23 - jhs] 정상 결과 반환 객체로 refactoring

--- src/main/java/com/dailydoingdeed/web/PostsApiController.java
+++ src/main/java/com/dailydoingdeed/web/PostsApiController.java
@@ -1,5 +1,7 @@
 package com.dailydoingdeed.web;
 
+import com.dailydoingdeed.global.response.common.ListResponseData;
+import com.dailydoingdeed.global.response.common.SingleResponseData;
 import com.dailydoingdeed.posts.PostsService;
 import com.dailydoingdeed.web.dto.PostsListResponseDto;
 import com.dailydoingdeed.web.dto.PostsResponseDto;
@@ -18,29 +20,29 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts") // @PostMapping("/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
-        return postsService.save(requestDto);
+    public SingleResponseData<Long> save(@RequestBody PostsSaveRequestDto requestDto){
+        return SingleResponseData.of(postsService.save(requestDto));
     }
 
     @PutMapping("/api/v1/posts/{id}") // @PatchMapping("/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
-        return postsService.update(id, requestDto);
+    public SingleResponseData<Long> update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return SingleResponseData.of(postsService.update(id, requestDto));
     }
 
     @DeleteMapping("/api/v1/posts/{id}") // @DeleteMapping("/posts/{id}")
-    public Long delete(@PathVariable Long id) {
+    public SingleResponseData<Long> delete(@PathVariable Long id) {
         postsService.delete(id);
-        return id;
+        return SingleResponseData.of(id);
     }
 
     @GetMapping("/api/v1/posts/{id}") // @GetMapping("/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id){
-        return postsService.findById(id);
+    public SingleResponseData<PostsResponseDto> findById(@PathVariable Long id){
+        return SingleResponseData.of(postsService.findById(id));
     }
 
     // 모든 리스트 가져오기 api (테스트 작성)
     @GetMapping("/api/v1/posts/all") // @GetMapping("/posts/all")
-    public List<PostsListResponseDto> all(){
-        return postsService.findAllDesc();
+    public ListResponseData<PostsListResponseDto> all(){
+        return ListResponseData.of(postsService.findAllDesc());
     }
 }
