273ad55bf90aa519b4e65b6e25cbb60f45f66d21
JHS <wnghdtjr129@naver.com>
2022-09-23 14:24:27 +0900

    [22.09.23 - jhs] 정상 값 반환 객체 생성

---
+++ src/main/java/com/dailydoingdeed/global/response/common/ListResponseData.java
@@ -0,0 +1,18 @@
+package com.dailydoingdeed.global.response.common;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ListResponseData<T> extends ResponseData {
+    private List<T> data;
+
+    private ListResponseData(List<T> data) {
+        this.data = data;
+    }
+
+    public static <T> ListResponseData of(List<T> data) {
+        return new ListResponseData(data);
+    }
+}
