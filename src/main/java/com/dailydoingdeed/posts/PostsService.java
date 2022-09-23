f396abc1f6a2389f92a5abc35ab5846699e673ca
JHS <wnghdtjr129@naver.com>
2022-09-23 14:25:40 +0900

    [22.09.23 - jhs] Service Layer CustomException 적용

--- src/main/java/com/dailydoingdeed/posts/PostsService.java
+++ src/main/java/com/dailydoingdeed/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.dailydoingdeed.posts;
 
 import com.dailydoingdeed.domain.posts.Posts;
 import com.dailydoingdeed.domain.posts.PostsRepository;
+import com.dailydoingdeed.global.response.error.exception.EntityNotFoundException;
 import com.dailydoingdeed.web.dto.PostsListResponseDto;
 import com.dailydoingdeed.web.dto.PostsResponseDto;
 import com.dailydoingdeed.web.dto.PostsSaveRequestDto;
@@ -19,23 +20,22 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
 
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestDto){
-        Posts posts = postsRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
-
-        posts.update(requestDto.getTitle(), requestDto.getContent());
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        postsRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("해당 게시글이 없습니다. id=" + id))
+                .update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
 
     @Transactional
-    public void delete (Long id) {
+    public void delete(Long id) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
@@ -43,9 +43,9 @@ public class PostsService {
     }
 
     @Transactional(readOnly = true)
-    public PostsResponseDto findById (Long id){
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
