281854ae6d244253070cec056be65de5321ad22c
JHS <wnghdtjr129@naver.com>
2022-10-07 13:59:40 +0900

    [22.10.07 - jhs] 기타 설정 추가(필터, passwordEncoder, dependency)

--- src/main/java/com/dailydoingdeed/config/auth/SecurityConfig.java
+++ src/main/java/com/dailydoingdeed/config/auth/SecurityConfig.java
@@ -1,34 +1,53 @@
 package com.dailydoingdeed.config.auth;
 
+import com.dailydoingdeed.config.auth.jwt.JwtAuthenticationFilter;
+import com.dailydoingdeed.config.auth.jwt.JwtAuthorizationFilter;
 import com.dailydoingdeed.domain.user.Role;
+import com.dailydoingdeed.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
 
-@RequiredArgsConstructor
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final UserRepository userRepository;
+    private final CorsFilter corsFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .headers().frameOptions().disable()
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
-                .antMatchers("/docs/**").permitAll() //api docs 보기 허용
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                .anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .and()
+                .addFilter(corsFilter)
+                .addFilter(new JwtAuthenticationFilter(authenticationManagerBean()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManagerBean(), userRepository));
+
+        http.csrf()
+                .disable()
+            .authorizeRequests()
+                .antMatchers("/posts/**").hasRole(Role.USER.name())
+                .anyRequest().permitAll()
+            .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .logout().logoutSuccessUrl("/")
+            .and()
                 .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+                    .userInfoEndpoint()
+                    .userService(customOAuth2UserService)
+            .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler);
+
     }
 }
